2a2d91235e311e8beb7b63bd8858996ba1f2b328
darengarneaudionne <2065779@csfoy.ca>
2024-01-06 22:37:37 -0500

    test

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+</head>
+
+
+</html>
+<?php
+// Redirection en PHP
+header("Location: /public/");
+exit; // Assure que le script s'arrête ici pour éviter toute exécution supplémentaire
+?>
