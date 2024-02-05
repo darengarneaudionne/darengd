659fe247f63bf74b2c83cb70b8e1c1d20640f1aa
darengarneaudionne <98491387+darengarneaudionne@users.noreply.github.com>
2024-02-05 10:03:39 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,6 @@
 <head>
     <meta charset="utf-8">
 </head>
-
+hellooo
 
 </html>
-<?php
-// Redirection en PHP
-header("Location: /public/");
-exit; // Assure que le script s'arrête ici pour éviter toute exécution supplémentaire
-?>
